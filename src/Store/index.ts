c6214e9fd68d6e2c9dfca709e14031552426736e
Денис Вильданов <denisvildanov@dvav.local>
2019-10-06 15:35:45 +0300

    Redux store some logic

--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,2 +1,25 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
+
+const allReducers = combineReducers({
+
+});
+
+const someActionsListener = (state: any, action: any) => {
+    // if () {}
+    // some logic here
+    // for example logout:
+    // if (action.type === 'LOGOUT) {
+        // const stateKiller = undefined;
+        // return allReducers(stateKiller, action);
+    // }
+
+    return allReducers(state, action);
+}
+
+export const store = createStore(
+    someActionsListener,
+    compose(
+        applyMiddleware(thunk),
+    )
+)
